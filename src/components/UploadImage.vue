dea2ea90057e51af021d1a3423bbd016aa8e77ce
YCHE0392 <stanpoldark@gmail.com>
2024-05-27 17:30:16 +1000

    Update UploadImage.vue

--- src/components/UploadImage.vue
+++ src/components/UploadImage.vue
@@ -25,7 +25,7 @@ export default {
 
       const formData = new FormData();
       formData.append('file', this.file); 
-
+      console.log(formData);
       axios.post('https://t57wg2rlb3.execute-api.us-east-1.amazonaws.com/prod/api/upload', formData, {
         headers: {
           'Content-Type': 'multipart/form-data'
