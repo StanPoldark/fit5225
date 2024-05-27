1b4cf2f725f40fa6cd42abcc80ad130f001672bd
YCHE0392 <stanpoldark@gmail.com>
2024-05-27 19:50:34 +1000

    Update UploadImage.vue

--- src/components/UploadImage.vue
+++ src/components/UploadImage.vue
@@ -20,33 +20,23 @@ export default {
     onFileChange(e) {
       this.file = e.target.files[0];
     },
-    convertImageToBase64(file, callback) {
-      const reader = new FileReader();
-      reader.onload = (e) => {
-        // Directly use the result from FileReader
-        const base64String = e.target.result;
-        callback(base64String);
-      };
-      reader.readAsDataURL(file);
-    },
     uploadFile() {
       if (!this.file) return;
 
-      // Reference `this.convertImageToBase64` and `this.file` correctly
-      this.convertImageToBase64(this.file, (base64String) => {
-        axios.post('https://t57wg2rlb3.execute-api.us-east-1.amazonaws.com/prod/api/upload', {
-            image: base64String  // Send the Base64-encoded image data
-        }, {
-            headers: {
-                'Content-Type': 'application/json'
-            }
-        })
-        .then(response => {
-            console.log('Upload successful:', response.data);
-        })
-        .catch(error => {
-            console.error('Upload failed:', error.response.data);
-        });
+      const formData = new FormData();
+      formData.append('file', this.file); 
+      axios.post('https://t57wg2rlb3.execute-api.us-east-1.amazonaws.com/prod/api/upload', formData, {
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      })
+      .then(response => {
+        console.log('File uploaded successfully:', response.data);
+     
+      })
+      .catch(error => {
+        console.error('Error uploading file:', error);
+       
       });
     }
   }
