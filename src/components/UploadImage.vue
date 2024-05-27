43e6741d88e21aed6057a6c802e5013a8ac56b41
YCHE0392 <stanpoldark@gmail.com>
2024-05-27 18:44:33 +1000

    Update UploadImage.vue

--- src/components/UploadImage.vue
+++ src/components/UploadImage.vue
@@ -23,18 +23,33 @@ export default {
     uploadFile() {
       if (!this.file) return;
 
-      const formData = new FormData();
-      formData.append('file', this.file); 
-      axios.post('https://t57wg2rlb3.execute-api.us-east-1.amazonaws.com/prod/api/upload', formData)
-      .then(response => {
-        console.log('File uploaded successfully:', response.data);
-     
-      })
-      .catch(error => {
-        console.error('Error uploading file:', error);
-       
-      });
+      convertImageToBase64(file, function(base64String) {
+        axios.post('your-api-endpoint', {
+            image: base64String  
+        }, {
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+        .then(response => {
+            console.log('Upload successful:', response.data);
+        })
+        .catch(error => {
+            console.error('Upload failed:', error);
+        });
+    });
+
     }
   }
 };
+
+function convertImageToBase64(file, callback) {
+    const reader = new FileReader();
+    reader.onload = function(e) {
+        const base64String = e.target.result.split(',')[1];
+        callback(base64String);
+    };
+    reader.readAsDataURL(file);
+}
+
 </script>
